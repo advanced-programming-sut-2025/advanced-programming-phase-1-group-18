2a72485ebfa1a8db2b3f7982d25d9a5796d8ff10
Samyar <samyar.math2006@gmail.com>
2025-04-21 01:13:23 +0330

    RegisterCommands

--- RegisterMenuCommands.java
+++ RegisterMenuCommands.java
@@ -5,17 +5,17 @@ import java.util.regex.Pattern;
 
 public enum RegisterMenuCommands {
     MenuEnter("\\s*menu\\s+enter\\s+(?<menuName>.*\\S)\\s*"),
-    MenuExit(""),
+    MenuExit("\\s*menu\\s+exit\\s*"),
     ShowCurrentMenu("show\\s+current\\s+menu"),
-    Register("");
-
+    Register("\\s*register\\s+-u\\s+(?<username>\\w{3,})\\s+-p\\s+(?<password>(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/\\\\|]).{8,})\\s+(?<passwordConfirm>\\S+)\\s+-n\\s+(?<nickname>\\S+)\\s+-e\\s+(?<email>[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,})\\s+-g\\s+(?<gender>male|female)\\s*"),
+    PickQuestion("Salam-Aleikom-Soosan-Khanoom");
     private final String pattern;
 
     RegisterMenuCommands(String pattern) {
         this.pattern = pattern;
     }
 
-    public Matcher getMather(String input) {
+    public Matcher getMatcher(String input) {
         Matcher matcher = Pattern.compile(this.pattern).matcher(input);
 
         if (matcher.matches()) {
