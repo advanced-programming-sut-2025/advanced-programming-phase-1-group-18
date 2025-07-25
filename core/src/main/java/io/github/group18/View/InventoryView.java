6177a751b742977f3662b91d6de114b0a8b5a19b
Samyar Safari <samymath20.06@gmail.com>
2025-07-25 20:29:19 +0330

    added Friend button (F)

--- core/src/main/java/io/github/group18/View/InventoryView.java
+++ core/src/main/java/io/github/group18/View/InventoryView.java
@@ -17,10 +17,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import io.github.group18.Model.App;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import io.github.group18.Model.Game;
 import io.github.group18.Model.GameAssetManager;
 
 import javax.swing.event.ChangeEvent;
 
+
+import java.util.Objects;
 
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
@@ -188,7 +191,7 @@ public class InventoryView {
 
         TextureRegion playerFrontFrame = GameView.getPlayerFrontImage();
         Image playerImage = new Image(new TextureRegionDrawable(playerFrontFrame));
-        Label nameLabel = new Label("Name: " + App.getCurrentUser().getUsername(), skin);
+        Label nameLabel = new Label("        Name: " + Game.getCurrentPlayer().getOwner().getUsername(), skin);
         nameLabel.setColor(Color.GOLD);
         nameLabel.setFontScale(0.75f);
 
@@ -276,15 +279,14 @@ public class InventoryView {
 
     private int getSkillValue(int idx) {
         return switch (idx) {
-            case 0 -> 40;
-            case 1 -> 25;
-            case 2 -> 8;
-            case 3 -> 0;
-            case 4 -> 33;
+            case 0 -> Objects.requireNonNull(Game.getCurrentPlayer()).getFarmingSkill().getLevel();
+            case 1 -> Objects.requireNonNull(Game.getCurrentPlayer()).getFishingSkill().getLevel();
+            case 2 -> Objects.requireNonNull(Game.getCurrentPlayer()).getForagingSkill().getLevel();
+            case 3 -> Objects.requireNonNull(Game.getCurrentPlayer()).getMiningSkill().getLevel();
+            case 4 -> 0;
             default -> 0;
         };
     }
-
 
 
     private void showInventoryTab() {
